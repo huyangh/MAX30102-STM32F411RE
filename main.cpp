ff382379acdaf6a8de65072e9fb3c989a78be398
huyangh <huyangh@sina.com>
2018-03-28 12:36:32 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,197 @@
+/** \file main.cpp ******************************************************
+*
+* Project: STM32F103C8T6+MAX30102
+* Edited by Anning
+* Shop website: https://shop108071095.taobao.com
+* Email: anning865@126.com
+* ------------------------------------------------------------------------- */
+/*******************************************************************************
+* Copyright (C) 2016 Maxim Integrated Products, Inc., All Rights Reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
+* OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*
+* Except as contained in this notice, the name of Maxim Integrated
+* Products, Inc. shall not be used except as stated in the Maxim Integrated
+* Products, Inc. Branding Policy.
+*
+* The mere transfer of this software does not imply any licenses
+* of trade secrets, proprietary technology, copyrights, patents,
+* trademarks, maskwork rights, or any other form of intellectual
+* property whatsoever. Maxim Integrated Products, Inc. retains all
+* ownership rights.
+*******************************************************************************
+*/
+/*!\mainpage Main Page
+*
+* \section intro_sec Introduction
+*
+* This is the code documentation for the MAXREFDES117# subsystem reference design.
+* 
+*  The Files page contains the File List page and the Globals page.
+* 
+*  The Globals page contains the Functions, Variables, and Macros sub-pages.
+*
+* \image html MAXREFDES117_Block_Diagram.png "MAXREFDES117# System Block Diagram"
+* 
+* \image html MAXREFDES117_firmware_Flowchart.png "MAXREFDES117# Firmware Flowchart"
+*
+*/
+#include "mbed.h"
+#include "algorithm.h"
+#include "MAX30102.h"
+
+#define MAX_BRIGHTNESS 255
+
+uint32_t aun_ir_buffer[500]; //IR LED sensor data
+int32_t n_ir_buffer_length;    //data length
+uint32_t aun_red_buffer[500];    //Red LED sensor data
+int32_t n_sp02; //SPO2 value
+int8_t ch_spo2_valid;   //indicator to show if the SP02 calculation is valid
+int32_t n_heart_rate;   //heart rate value
+int8_t  ch_hr_valid;    //indicator to show if the heart rate calculation is valid
+uint8_t uch_dummy;
+
+Serial pc(SERIAL_TX, SERIAL_RX);    //initializes the serial port, TX-PA2, RX-PA3
+
+PwmOut pwmled(PB_3);  //initializes the pwm output PB3 that connects to the LED
+DigitalIn INT(PC_8);  //pin PB9 connects to the interrupt output pin of the MAX30102
+DigitalOut led(PA_5); //PA5 connects to the on board user LED
+
+// the setup routine runs once when you press reset:
+int main() { 
+    uint32_t un_min, un_max, un_prev_data;  //variables to calculate the on-board LED brightness that reflects the heartbeats
+    int i;
+    int32_t n_brightness;
+    float f_temp;
+    
+    maxim_max30102_reset(); //resets the MAX30102
+    // initialize serial communication at 115200 bits per second:
+    pc.baud(115200);
+    pc.format(8,SerialBase::None,1);
+    wait(1);
+    
+    //read and clear status register
+    maxim_max30102_read_reg(0,&uch_dummy);
+          
+    
+     //wait unti                                                                                                                                                                                                                                                                                         `l the user presses a key
+    while(pc.readable()==0)
+    {
+        pc.printf("\x1B[2J");  //clear terminal program screen
+        pc.printf("Press any key to start conversion\n\r");
+        wait(1);
+    }
+    uch_dummy=getchar();
+    
+    maxim_max30102_init();  //initializes the MAX30102
+        
+        
+    n_brightness=0;
+    un_min=0x3FFFF;
+    un_max=0;
+  
+    n_ir_buffer_length=500; //buffer length of 100 stores 5 seconds of samples running at 100sps
+    
+    //read the first 500 samples, and determine the signal range
+    for(i=0;i<n_ir_buffer_length;i++)
+    {
+        while(INT.read()==1);   //wait until the interrupt pin asserts
+        
+        maxim_max30102_read_fifo((aun_red_buffer+i), (aun_ir_buffer+i));  //read from MAX30102 FIFO
+            
+        if(un_min>aun_red_buffer[i])
+            un_min=aun_red_buffer[i];    //update signal min
+        if(un_max<aun_red_buffer[i])
+            un_max=aun_red_buffer[i];    //update signal max
+        pc.printf("red=");
+        pc.printf("%i", aun_red_buffer[i]);
+        pc.printf(", ir=");
+        pc.printf("%i\n\r", aun_ir_buffer[i]);
+    }
+    un_prev_data=aun_red_buffer[i];
+    
+    
+    //calculate heart rate and SpO2 after first 500 samples (first 5 seconds of samples)
+    maxim_heart_rate_and_oxygen_saturation(aun_ir_buffer, n_ir_buffer_length, aun_red_buffer, &n_sp02, &ch_spo2_valid, &n_heart_rate, &ch_hr_valid); 
+    
+    //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
+    while(1)
+    {
+        i=0;
+        un_min=0x3FFFF;
+        un_max=0;
+        
+        //dumping the first 100 sets of samples in the memory and shift the last 400 sets of samples to the top
+        for(i=100;i<500;i++)
+        {
+            aun_red_buffer[i-100]=aun_red_buffer[i];
+            aun_ir_buffer[i-100]=aun_ir_buffer[i];
+            
+            //update the signal min and max
+            if(un_min>aun_red_buffer[i])
+            un_min=aun_red_buffer[i];
+            if(un_max<aun_red_buffer[i])
+            un_max=aun_red_buffer[i];
+        }
+        
+        //take 100 sets of samples before calculating the heart rate.
+        for(i=400;i<500;i++)
+        {
+            un_prev_data=aun_red_buffer[i-1];
+            while(INT.read()==1);
+            maxim_max30102_read_fifo((aun_red_buffer+i), (aun_ir_buffer+i));
+        
+            if(aun_red_buffer[i]>un_prev_data)//just to determine the brightness of LED according to the deviation of adjacent two AD data
+            {
+                f_temp=aun_red_buffer[i]-un_prev_data;
+                f_temp/=(un_max-un_min);
+                f_temp*=MAX_BRIGHTNESS;
+                n_brightness-=(int)f_temp;
+                if(n_brightness<0)
+                    n_brightness=0;
+            }
+            else
+            {
+                f_temp=un_prev_data-aun_red_buffer[i];
+                f_temp/=(un_max-un_min);
+                f_temp*=MAX_BRIGHTNESS;
+                n_brightness+=(int)f_temp;
+                if(n_brightness>MAX_BRIGHTNESS)
+                    n_brightness=MAX_BRIGHTNESS;
+            }
+
+            pwmled.write(1-(float)n_brightness/256);//pwm control led brightness
+                        if(n_brightness<120)
+                            led=1;
+                        else
+                            led=0;
+
+            //send samples and calculation result to terminal program through UART
+            pc.printf("red=");
+            pc.printf("%i", aun_red_buffer[i]);
+            pc.printf(", ir=");
+            pc.printf("%i", aun_ir_buffer[i]);
+            pc.printf(", HR=%i, ", n_heart_rate); 
+            pc.printf("HRvalid=%i, ", ch_hr_valid);
+            pc.printf("SpO2=%i, ", n_sp02);
+            pc.printf("SPO2Valid=%i\n\r", ch_spo2_valid);
+        }
+        maxim_heart_rate_and_oxygen_saturation(aun_ir_buffer, n_ir_buffer_length, aun_red_buffer, &n_sp02, &ch_spo2_valid, &n_heart_rate, &ch_hr_valid); 
+    }
+}
